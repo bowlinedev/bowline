cfc49d99ef056363919592955247d2d37518d116
tunardev <tunardev@gmail.com>
2026-09-16 00:15:20 +0400

    feat(mock): add deterministic fake data generator
    Signed-off-by: tunardev <tunardev@gmail.com>

---
+++ cmd/bowline/internal/fake/words.go
@@ -0,0 +1,12 @@
+package fake
+
+var words = []string{
+	"alpha", "amber", "anchor", "apple", "arrow", "atlas", "aurora", "basil", "beacon", "birch",
+	"blossom", "breeze", "canyon", "cedar", "cinder", "clover", "cobalt", "comet", "coral", "crest",
+	"dawn", "delta", "drift", "dune", "ember", "falcon", "fern", "flint", "forest", "garnet",
+	"glade", "grove", "harbor", "hazel", "heron", "ivory", "jade", "juniper", "kestrel", "lagoon",
+	"lantern", "lark", "linen", "lotus", "maple", "marble", "meadow", "mesa", "mist", "nectar",
+	"nova", "oak", "opal", "orchid", "pebble", "pine", "prairie", "quartz", "raven", "reed",
+	"ridge", "river", "saffron", "sage", "sierra", "slate", "sparrow", "spruce", "summit", "thistle",
+	"tide", "topaz", "tundra", "umber", "vale", "velvet", "willow", "wren", "zephyr", "zinc",
+}
