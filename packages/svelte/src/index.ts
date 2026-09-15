b41af2bc32877aec261cb6a4ec06cba1e7d85d13
tunardev <tunardev@gmail.com>
2026-09-16 00:52:16 +0400

    feat(svelte): add store bindings and SvelteKit helpers
    Signed-off-by: tunardev <tunardev@gmail.com>
    (cherry picked from commit 5c1d46fd630c90399fff589fd8d9853566a61461)

---
+++ packages/svelte/src/index.ts
@@ -0,0 +1,14 @@
+export { serverClient } from "./kit.js";
+export type {
+  BowlineStores,
+  MutationHandle,
+  MutationState,
+  MutationStores,
+  QueryOptions,
+  QueryState,
+  QueryStore,
+  QueryStores,
+} from "./stores.js";
+export { bowlineStores } from "./stores.js";
+
+export const version = "0.4.0";
